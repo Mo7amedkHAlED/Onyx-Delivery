9724a5d728b83546fa5fedf038015bffd85fa96b
Mo7amedkHAlED <>
2023-08-14 03:38:02 +0300

    implement Login screen UI

---
+++ Onyx-Delivery/Presentation/LoginV/View/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Onyx-Delivery
+//
+//  Created by Mohamed Khaled on 14/08/2023.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet weak var userIDTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
