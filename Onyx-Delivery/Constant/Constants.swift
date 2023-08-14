4b630da08be1abc0cb183e67e89d8198c2bdf147
Mo7amedkHAlED <>
2023-08-14 17:05:37 +0300

    add constants enum that holds app constants values

---
+++ Onyx-Delivery/Constant/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Onyx-Delivery
+//
+//  Created by Mohamed Khaled on 14/08/2023.
+//
+
+import Foundation
+enum Constants {
+    static let baseUrl: String = "http://mapp.yemensoft.net/"
+    static let checkDeliveryLogin = "OnyxDeliveryService/Service.svc/CheckDeliveryLogin"
+    static let getDeliveryBillsItems = "OnyxDeliveryService/Service.svc/GetDeliveryBillsItems"
+    static let LanguageNU: String = "2"
+}
