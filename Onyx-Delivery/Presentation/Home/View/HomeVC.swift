006c483272dbba99e560f9cb4be10ba16393c7d3
Mo7amedkHAlED <>
2023-08-15 20:15:56 +0300

    implement home screen UI

--- Onyx-Delivery/Presentation/Home/View/HomeVC.swift
+++ Onyx-Delivery/Presentation/Home/View/HomeVC.swift
@@ -10,6 +10,11 @@ import RxCocoa
 import RxSwift
 
 class HomeVC: UIViewController {
+    // MARK: - @IBOutlet
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: - Variables
     private let bag = DisposeBag()
     var viewModel: HomeViewModel
@@ -26,11 +31,46 @@ class HomeVC: UIViewController {
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNibFileTableView()
         setNavigationController(true)
+        subscribeToSegmentedControl()
+        segmentedControl.setOldLayout(tintColor: UIColor(named: "#004F62") ?? .red )
+        configureNibFileTableView()
+        tableViewSubscription()
+        viewModel.viewDidLoad()
     }
     
     // MARK: - set Navigation Bar Hidden
     private func setNavigationController(_ isHidden: Bool ) {
         navigationController?.setNavigationBarHidden(isHidden, animated: true)
+    }
+    // MARK: - configure Nib File TableView
+    private func configureNibFileTableView() {
+        tableView.register(UINib(nibName: String(describing: OrderDetailsTableViewCell.self), bundle: nil),
+                                  forCellReuseIdentifier: String(describing: OrderDetailsTableViewCell.self))
+    }
+    // MARK: - table View Subscription
+    private func tableViewSubscription() {
+        viewModel.deliveryBill.bind(to: tableView.rx.items(cellIdentifier: String(describing: OrderDetailsTableViewCell.self), cellType: OrderDetailsTableViewCell.self)) { (_ , element , cell ) in
+            cell.selectionStyle = .none
+            cell.setupCell(element)
+        }.disposed(by: bag)
+    }
+    
+    // MARK: - subscribe To Segmented Control
+    private func subscribeToSegmentedControl() {
+        segmentedControl.rx.selectedSegmentIndex.observe(on: MainScheduler.instance).subscribe(onNext: { [weak self]selectedIndex in
+            guard let self = self else {return}
+            switch selectedIndex {
+            case 0:
+                self.viewModel.tag = 1
+                self.viewModel.GetDeliveryBillsItems()
+            case 1:
+                self.viewModel.tag = 2
+                self.viewModel.GetDeliveryBillsItems()
+            default:
+                break
+            }
+        }).disposed(by: bag)
     }
 }
--- Onyx-Delivery/Presentation/Home/ViewModel/HomeViewModel.swift
+++ Onyx-Delivery/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -6,6 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
 class HomeViewModel {
+    private let homeUseCase: HomeUseCase
+    private let bag = DisposeBag()
+    var deliveryBill: BehaviorRelay<[DeliveryBill]> = .init(value: [])
+    var tag = 1
+    init(homeUseCase: HomeUseCase = HomeUseCaseImple()) {
+        self.homeUseCase = homeUseCase
+    }
+    // MARK: - viewDidLoad
+    func viewDidLoad() {
+        GetDeliveryBillsItems()
+    }
     
+    func GetDeliveryBillsItems() {
+        homeUseCase.GetDeliveryBillsItems().subscribe(onNext: { [weak self] data in
+            guard let self = self else { return }
+            switch self.tag == 1 {
+            case true:
+                let filteredBillsWithFlag0 = data.Data?.DeliveryBills.filter { $0.DLVRY_STATUS_FLG == "0" } ?? []
+                self.deliveryBill.accept(filteredBillsWithFlag0)
+                
+            case false:
+                let filteredBillsWithFlagOther = data.Data?.DeliveryBills.filter { $0.DLVRY_STATUS_FLG != "0" } ?? []
+                self.deliveryBill.accept(filteredBillsWithFlagOther)
+            }
+        }).disposed(by: bag)
+    }
 }
